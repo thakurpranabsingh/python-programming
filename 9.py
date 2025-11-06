a12f8f3792fc5da96ece88cdd8770ad8463a6c83
thakurpranabsingh <thakurpranabsingh94@gmail.com>
2025-11-06 11:14:30 +0530

    Add examples of break, continue, and pass statements

---
+++ 9.py
@@ -0,0 +1,12 @@
+for i in range(1, 6):
+    if i == 3:
+        break
+    print("Break example:", i)
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print("Continue example:", i)
+for i in range(1, 6):
+    if i == 3:
+        pass
+    print("Pass example:", i)
